6ecbca54a13719424a50f8f774408231c58f4d16
Aman Mittal <amandeepmittal@live.com>
2019-05-07 16:35:30 +0530

    Link

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { BrowserRouter as Router, Route } from "react-router-dom"
+import { BrowserRouter as Router, Route, Link } from "react-router-dom"
 
 import Home from "./components/Home"
 import About from "./components/About"
@@ -8,6 +8,15 @@ function App() {
 	return (
 		<Router>
 			<div>
+				<nav style={{ margin: 10 }}>
+					<Link to='/' style={{ padding: 10 }}>
+						Home
+					</Link>
+
+					<Link to='/about' style={{ padding: 10 }}>
+						About
+					</Link>
+				</nav>
 				<Route path='/' exact component={Home} />
 				<Route path='/about' component={About} />
 			</div>
